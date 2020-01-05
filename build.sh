8c46e5e51ff13ca9427734e9d82a3cdf6259a652
Acrimon <joel.wejdenstal@gmail.com>
2020-01-05 21:28:20 +0100

    Merge opt and js steps.

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 mkdir build
 wasm-pack build
 terser pkg/argon2wasm.js -c -m > build/argon2.js
-wasm-opt -Oz pkg/argon2wasm_bg.wasm -o pkg/argon2wasm_bg_optimized.wasm
-wasm2js pkg/argon2wasm_bg_optimized.wasm > pkg/argon2_bg.js
+wasm2js -Oz pkg/argon2wasm_bg.wasm > pkg/argon2_bg.js
 terser pkg/argon2_bg.js -c -m > build/argon2_bg.js
